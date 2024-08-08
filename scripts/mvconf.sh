f27235e55f9048088443d82fbc8cc3d94e2a7f07
gentoo <...>
2024-08-08 20:16:44 +0200

    mv the scripts to the directory

---
+++ scripts/mvconf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#check if dir´s exist 
+
+#copy conf files
+cp ../zshrc ~/.zshrc
+cp ../p10k ~/.p10k.zsh
+cp ../vimrc ~/.vimrc
+cp ../alacritty.toml ~/.config/alacritty.toml
+#cp config.conf $(HOME)/.config/neofetch/config.conf
