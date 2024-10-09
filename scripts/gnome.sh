22bd9525a031b4d5edb91e333287f8fb6022c6a9
Jan Giritsch <jangiritsch@MacBook-Air-von-Jan.fritz.box>
2024-10-09 20:57:52 +0200

    add keybind for terminal

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -6,7 +6,7 @@ CURSOR_THEME=" "
 GTK_THEME=" "
 WALLPAPPER=" "
 
-# download the themes
+# download the themes or i add a copy to the repo
 
 #install the themes
 mkdir -p $HOME/.themes $HOME/.icons
@@ -20,3 +20,13 @@ gsettings set org.gnome.desktop.interface cursor-theme $CURSOR_THEME
 
 #install some extensions
 gnome-extensions install dash-to-dock 
+
+# set keybinds
+# creat keybind paht 
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# set shortcut for my terminal
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command '/usr/bin/alacritty'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>+'
+
