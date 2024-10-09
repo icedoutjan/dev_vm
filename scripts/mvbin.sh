ef7e310609991c715e6ea7daa720f93215b6650e
Jan Giritsch <jangiritsch@2506ee0d-58c0-43a2-88de-94df7c7ac5c9.fritz.box>
2024-10-09 20:00:28 +0200

    add the battery script

--- scripts/mvbin.sh
+++ scripts/mvbin.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+BINPATH=$HOME/.local/bin/
+
 #init the local bin dir
-
+mkdir -p $BINPATH
 #copy the executabils 
+gcc ../bin/battery.cpp -O3 -o $BINPATH/battery 
 
-#make sure for the right permissions 
+#make sure for the right permissions
+chmod +x $BINPATH/*
