b61fdb4c5e53ada6b19c9540472682eb89ebea6a
icedoutjan <120628427+icedoutjan@users.noreply.github.com>
2024-10-14 20:59:41 +0200

    add a man page tool that I often use

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -2,7 +2,7 @@
 
 # is maybe not the best way but i start here to write down wich packes i like
 if [ $whoami == "root" ];then
-        pacman -S --noconfirm --quiet cmake grc fastfetch pacman-contrib btop vim alacritty yazi 
+        pacman -S --noconfirm --quiet cmake grc fastfetch pacman-contrib btop vim alacritty yazi tldr
 else
         echo "run as root"
 fi
