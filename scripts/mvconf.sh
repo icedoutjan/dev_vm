11d0ca00795ae780ab9d6a81e2d244bae285d4ae
jan <jan>
2025-03-19 17:46:44 +0100

    add a tty zsh theme and update the mvconf to install it

--- scripts/mvconf.sh
+++ scripts/mvconf.sh
@@ -7,4 +7,6 @@ cp ../conf/zshrc ~/.zshrc
 cp ../conf/p10k.zsh ~/.p10k.zsh
 cp ../conf/vimrc ~/.vimrc
 cp ../conf/alacritty.toml ~/.config/alacritty.toml
+
+cp ../conf/tty.zsh-theme ~/.oh-my-zsh/themes/tty.zsh-theme
 #cp config.conf $(HOME)/.config/neofetch/config.conf
